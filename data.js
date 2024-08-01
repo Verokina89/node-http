e7f1744dc62988528a5cbb92b6eaabf515b69b3e
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-01 19:18:41 +0200

    maybe complete, problems terminal

---
+++ data.js
@@ -0,0 +1,10 @@
+const objectData = {
+    title: 'Blog de Programación',
+    subtitle: 'Recursos de Node.js',
+    description: 'Este es un espacio digital para compartir creaciones con node.js',
+    creator: 'Veronica I. Perez S.',
+    date: new Date().getFullYear(),
+
+}
+
+module.exports = objectData;
---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('node:http');
+const objectData = require('./data');
+
+const server = http.createServer((req, res) => {
+    console.log('request recived');
+    res.end(`
+            <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <main>
+            <h1>${objectData.title}</h1>
+            <h2>${objectData.subtile}</h2>
+            <p>${objectData.description}</p>
+        </main>
+        <footer>
+            <p><span>Creado por: ${objectData.creator}</span></p>
+            <p>Fecha: ${objectData.date}</p>
+        </footer>
+    </body>
+    </html>`);
+
+});
+
+server.listen(0, () => {
+    console.log(`
+        server listening on port http://localhost:${server.address().port}
+        `);
+});
